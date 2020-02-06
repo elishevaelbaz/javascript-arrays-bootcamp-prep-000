a880eec9eb93e5f139f0d17cc9c8c1bcd7ed4a3a
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-02-06 01:00:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ function addElementToBeginningOfArray(array, element){
 
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-
+   return array.unshift(element);
   return array.push(element);
 }
 
@@ -21,7 +21,7 @@ function addElementToEndOfArray(array, element){
 
 function destructivelyAddElementToEndOfArray(array, element){
 
-  return array.unshift(element);
+ 
 }
 
 
