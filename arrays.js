7f76eb2ca4d96d37a8fb60aa60ec381728ce4f99
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-02-06 00:57:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,40 @@ function destructivelyAddElementToEndOfArray(array, element){
 
   return array.unshift(element);
 }
+
+
+function accessElementInArray(array, index){
+  return array[i]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift(element);
+}
+    
+
+function removeElementFromBeginningOfArray(array){
+  var newArray = array.slice(1)
+  return newArray
+}
+  
+
+ function destructivelyRemoveElementFromEndOfArray(array){
+   return array.pop()
+ }
+  
+function removeElementFromEndOfArray(array){
+  var newArray = array.slice(-1)
+  return newArray
+}
+', () => {
+    it('removes the last element from the array', () => {
+      expect(removeElementFromEndOfArray([1, 2, 3])).to.eql([1, 2])
+    })
+    
+    it('does not alter the original array', () => {
+      const array = [1, 2, 3];
+      removeElementFromEndOfArray(array);
+      expect(array).to.eql([1, 2, 3]);
+    })
+  })
+})
