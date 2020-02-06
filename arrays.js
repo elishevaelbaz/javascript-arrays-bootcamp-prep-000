09a90a3c7d377d169ea936675991f35987bdc9d5
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-02-06 00:48:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,12 @@
 function addElementToBeginningOfArray(array, element){
 
-  
+  var newArray = [element, ...array] 
+  return newArray
 }
 
 
 
 function destructivelyAddElementToBeginningOfArray(array, element){
 
-  array.push(element);
+  return array.push(element);
 }
